e1fd13434a138153b4e51d9b7a6814a69b8e6af1
David Kotlirevsky <david.kotlirevsky@gmail.com>
2019-09-13 13:50:50 -0300

    Se implementa la resta

--- spec/rspec_example_spec.rb
+++ spec/rspec_example_spec.rb
@@ -9,5 +9,12 @@ describe 'RspecExample' do
           expect(RspecExample.new().suma(1,1)).to eq(2)
       end
     end
+    end
+  describe '#resta' do
+    context 'if I send tow values' do
+      it 'result the exact addtition of both' do
+          expect(RspecExample.new().resta(2,1)).to eq(1)
+      end
+    end
   end
 end
